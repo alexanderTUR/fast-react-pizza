479152ae8e0934d892e6762b9e571cd5f625916d
Alexander Tur <alexandertur.fe@gmail.com>
2024-01-23 10:01:41 +0100

    Added geolocation with redux thunk

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,8 +2,8 @@ import { formatCurrency } from '../../utils/helpers.js';
 import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { store } from '../../store.js';
-import { useSelector } from 'react-redux';
-import { getUsername } from '../user/userSlice.js';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAddress, getUsername } from '../user/userSlice.js';
 import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice.js';
 
 import { createOrder } from '../../services/apiRestaurant.js';
@@ -16,14 +16,23 @@ const isValidPhone = (str) =>
 
 export const CreateOrder = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
   const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
   const [withPriority, setWithPriority] = useState(false);
   const cart = useSelector(getCart);
-  const username = useSelector(getUsername);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
   const totalCartPrice = useSelector(getTotalCartPrice);
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
   const totalPrice = totalCartPrice + priorityPrice;
+
+  const isLoadingAddress = addressStatus === 'loading';
 
   if (!cart.length) return <EmptyCart />;
 
@@ -55,11 +64,37 @@ export const CreateOrder = () => {
           </div>
         </div>
 
-        <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
+        <div className='relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
           <label className='sm:basis-40'>Address</label>
           <div className='grow'>
-            <input className='input w-full' type='text' name='address' required />
+            <input
+              className='input w-full'
+              type='text'
+              name='address'
+              defaultValue={address}
+              disabled={isLoadingAddress}
+              required
+            />
+            {addressStatus === 'error' && (
+              <p className='mt-2 rounded-md bg-red-100 px-2 py-1 text-xs text-red-700'>
+                {errorAddress}
+              </p>
+            )}
           </div>
+          {!position.latitude && !position.longitude && (
+            <span className='z-1 absolute right-2 top-2'>
+              <Button
+                type='small'
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+                disabled={isLoadingAddress}
+              >
+                Get position
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className='mb-12 flex items-center gap-5'>
@@ -78,7 +113,16 @@ export const CreateOrder = () => {
 
         <div>
           <input type='hidden' name='cart' value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type='primary'>
+          <input
+            type='hidden'
+            name='position'
+            value={
+              position.latitude && position.longitude
+                ? `${position.latitude},${position.longitude}`
+                : ''
+            }
+          />
+          <Button disabled={isSubmitting || isLoadingAddress} type='primary'>
             {isSubmitting ? 'Placing the order' : `Order now for ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,5 +1,5 @@
 import { getAddress } from '../../services/apiGeocoding.js';
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const getPosition = () => {
   return new Promise(function (resolve, reject) {
@@ -7,7 +7,7 @@ const getPosition = () => {
   });
 };
 
-export const fetchAddress = async () => {
+export const fetchAddress = createAsyncThunk('user/fetchAddress', async () => {
   // 1) We get the user's geolocation position
   const positionObj = await getPosition();
   const position = {
@@ -21,10 +21,14 @@ export const fetchAddress = async () => {
 
   // 3) Then we return an object with the data that we are interested in
   return { position, address };
-};
+});
 
 const initialState = {
   username: '',
+  status: 'idle',
+  position: {},
+  address: '',
+  error: null,
 };
 
 const userSlice = createSlice({
@@ -35,6 +39,20 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+        state.status = 'idle';
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = 'error';
+        state.error = action.error.message;
+      }),
 });
 
 export const { updateName } = userSlice.actions;
